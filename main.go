46c51ad3b56e569f0329328d2b80cffc5745c1ae
Guillermo Colombero <guillecolo@gmail.com>
2021-06-16 17:39:22 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,20 +5,42 @@ import (
 	"log"
 	"net/http"
 	"os"
+  
+  	"github.com/gin-gonic/contrib/static"
+  	"github.com/gin-gonic/gin"
 )
 
 // Hello World API
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000" //localhost
-	}
+	// Set the router as the default one shipped with Gin
+	  router := gin.Default()
 
-	helloHandler := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Colgate de esta TanKO!")
-	}
+	  // Serve frontend static files
+	  router.Use(static.Serve("/", static.LocalFile("./views", true)))
 
-	http.HandleFunc("/TanKO", helloHandler)
+	  // Setup route group for the API
+	  api := router.Group("/api")
+	  {
+	    api.GET("/", func(c *gin.Context) {
+	      c.JSON(http.StatusOK, gin.H {
+		"message": "pong",
+	      })
+	    })
+	  }
+	
+	  // Start and run the server
+  	router.Run(":3000")
+	
+	//port := os.Getenv("PORT")
+	//if port == "" {
+	//	port = "8000" //localhost
+	//}
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	//helloHandler := func(w http.ResponseWriter, r *http.Request) {
+	//	io.WriteString(w, "Colgate de esta TanKO!")
+	//}
+
+	//http.HandleFunc("/TanKO", helloHandler)
+
+	//log.Fatal(http.ListenAndServe(":"+port, nil))
 }
