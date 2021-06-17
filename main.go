b6b3a2aa353e7cad452faba8753a9de38b0080dc
gcolombero <gcolombero@teco.com.ar>
2021-06-17 14:18:58 -0300

    First commit.

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
-  	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	  // Set the router as the default one shipped with Gin
-	  router := gin.Default()
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
 
-	  // Setup route group for the API
-	  api := router.Group("/api")
+	// Setup route group for the API
+	api := router.Group("/api")
 
-	  api.GET("/ping", func(c *gin.Context) {
-	     c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	    })
-	  })
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
 
-  	router.Run()
+	router.Run()
 }
