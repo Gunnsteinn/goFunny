26a9c2eecfb84f70d3e80a1599c0433e917e465a
Guillermo Colombero <guillecolo@gmail.com>
2021-06-17 09:24:20 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	//"io"
-	//"log"
 	"net/http"
-	//"os"
-  
-  	//"github.com/gin-gonic/contrib/static"
   	"github.com/gin-gonic/gin"
 )
 
-// Hello World API
 func main() {
-	  //port := os.Getenv("PORT")
-	
 	  // Set the router as the default one shipped with Gin
 	  router := gin.Default()
-
-	  // Serve frontend static files
-	  //router.Use(static.Serve("/", static.LocalFile("./views", true)))
 
 	  // Setup route group for the API
 	  api := router.Group("/api")
@@ -28,30 +17,6 @@ func main() {
 		"message": "pong",
 	    })
 	  })
-	
-	
-	  //api := router.Group("/api")
-	  //{
-	    //api.GET("/", func(c *gin.Context) {
-	      //c.JSON(http.StatusOK, gin.H {
-		//"message": "pong",
-	      //})
-	    //})
-	  //}
-	
-	  // Start and run the server
+
   	router.Run()
-	
-	//port := os.Getenv("PORT")
-	//if port == "" {
-	//	port = "8000" //localhost
-	//}
-
-	//helloHandler := func(w http.ResponseWriter, r *http.Request) {
-	//	io.WriteString(w, "Colgate de esta TanKO!")
-	//}
-
-	//http.HandleFunc("/TanKO", helloHandler)
-
-	//log.Fatal(http.ListenAndServe(":"+port, nil))
 }
